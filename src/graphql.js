a9e04854e19b4785d6ff4b707fb8f992502533b4
r1soX <vladislav-smolin@list.ru>
2022-11-14 18:46:02 +0700

    update

--- src/graphql.js
+++ src/graphql.js
@@ -30,7 +30,7 @@ const GraphQL = {
         let body = { variables };
         let Hash = (sha256Hash === '') ? Operation_Hashes[QueryName] : sha256Hash
         let proxyString = (proxy === '' || proxy === undefined || proxy === null) ? null : proxy
-
+        console.log(proxyString);
         if (!GraphQL.ClientID)
             throw "Please make sure to fill in a ClientID";
         
